49702511ed6340024e61371aace337afa3c6f03b
donghyun_daniel <donghyun.main@gmail.com>
2020-04-11 01:08:57 +0900

    Initial commit

---
+++ Sokovan/Assets/ItemBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    Renderer myRenderer;
+    public Color touchColor;
+    Color orgColor;
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        orgColor = myRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Endpoints")
+        {
+            myRenderer.material.color = touchColor;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Endpoints")
+        {
+            myRenderer.material.color = orgColor;
+        }
+    }
+}
---
+++ Sokovan/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody playerRigidbody;
+
+    float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Selected Key at Input Manger
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        float fallSpeed = playerRigidbody.velocity.y;
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        velocity = velocity * speed;
+        velocity.y = fallSpeed;
+        playerRigidbody.velocity = velocity;
+    }
+}
