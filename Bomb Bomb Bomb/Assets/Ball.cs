edbbe1e7024c4a2254fc149c23d8b3573cb9220d
donghyun_daniel <donghyun.main@gmail.com>
2020-04-13 23:42:08 +0900

    Bomb working

---
+++ Bomb Bomb Bomb/Assets/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public LayerMask whatIsProp;
+    public ParticleSystem explosionParticle;
+    public AudioSource explosionAudio;
+    public float maxDamage = 100f;
+    public float explosionForce = 100f;
+    public float lifeTime = 10f;
+    public float explosionRadius = 10f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, whatIsProp);
+        explosionParticle.transform.parent = null;
+        explosionParticle.Play();
+        explosionAudio.Play();
+        Destroy(explosionParticle.gameObject, explosionParticle.duration);
+        Destroy(gameObject);
+    }
+
+    float CalculateDamage(Vector3 targetPosition)
+        {
+            return 0.1f;
+        }
+}
