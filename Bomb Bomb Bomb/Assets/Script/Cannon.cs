0b06f728e8a1d3f8aff5240399899db45f010fdc
donghyun_daniel <donghyun.main@gmail.com>
2020-04-15 03:12:55 +0900

    Bomb Working

---
+++ Bomb Bomb Bomb/Assets/Script/Cannon.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cannon : MonoBehaviour
+{
+    public Rigidbody ball;
+    public Transform firePos;
+    public Slider powerSlider;
+    public AudioSource shootingAudio;
+    public AudioClip fireClip;
+    public AudioClip chargingClip;
+    public float minForce = 15f;
+    public float maxForce = 30f;
+    public float chargingTime = 1f;
+    float currentForce;
+    float chargeSpeed;
+    bool fired;
+
+    private void OnEnable()
+    {
+        currentForce = minForce;
+        powerSlider.value = minForce;
+        fired = false;
+    }
+    private void Start()
+    {
+        chargeSpeed = (maxForce - minForce) / chargingTime;
+    }
+
+    private void Update()
+    {
+        if (fired == true)
+        {
+            return;
+        }
+        powerSlider.value = minForce;
+        if (currentForce >= maxForce && !fired)
+        {
+            currentForce = maxForce;
+            Fire();
+        }
+        else if (Input.GetButtonDown("Fire1"))
+        {
+            currentForce = minForce;
+            shootingAudio.clip = chargingClip;
+            shootingAudio.Play();
+        }
+        else if (Input.GetButton("Fire1") && !fired)
+        {
+            currentForce = currentForce + chargeSpeed * Time.deltaTime;
+            powerSlider.value = currentForce;
+        }
+        else if (Input.GetButtonUp("Fire1") && !fired)
+        {
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        fired = true;
+        Rigidbody ballInstance = Instantiate(ball, firePos.position, firePos.rotation);
+        ballInstance.velocity = currentForce * firePos.forward;
+        shootingAudio.clip = fireClip;
+        shootingAudio.Play();
+        currentForce = minForce;
+        Debug.Log("Fire!");
+    }
+}
