e30489fd94b9431402d5206f97bf4dc43004157a
donghyun_daniel <donghyun.main@gmail.com>
2020-04-23 05:01:11 +0900

    prac

---
+++ Practice/Assets/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public UnityEvent onPlayerDead;
+    public void Dead()
+    {
+        onPlayerDead.Invoke();
+        Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Dead();
+    }
+}
---
+++ Practice/Assets/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text playerStateText;
+    // Start is called before the first frame update
+    public void OnPlayerDead()
+    {
+        playerStateText.text = "You Die!";
+
+    }
+}
