2fd8b80b397bc89c764a083850c2ac92451532f0
donghyun_daniel <donghyun.main@gmail.com>
2020-04-24 19:21:07 +0900

    prac updated

---
+++ Practice/Assets/Booster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour
+{
+    public void HealthBoost(Character target)
+    {
+        Debug.Log(target.playerName + "의 체력 강화!");
+        target.hp += 10;
+    }
+
+    public void SheildBoost(Character target)
+    {
+        Debug.Log(target.playerName + "의 방어력 강화!");
+        target.defense += 10;
+    }
+
+    public void DmgBoost(Character target)
+    {
+        Debug.Log(target.playerName + "의 공격력 강화!");
+        target.dmg += 10;
+    }
+}
